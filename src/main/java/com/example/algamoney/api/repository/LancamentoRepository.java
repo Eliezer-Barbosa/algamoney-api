d302c9ed117d3a5fdc13e5c39de058ff0f0af59b
Eliezer <eliezer1789j@gmail.com>
2020-01-07 23:46:09 +0000

    implementando a paginação de lançamentos

--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -1,14 +1,10 @@
 package com.example.algamoney.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Lancamento;
-import com.example.algamoney.api.repository.filter.LancamentoFilter;
+import com.example.algamoney.api.repository.lancamento.LancamentoRepositoryQuery;
 
-public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
-
-	List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long>, LancamentoRepositoryQuery {
 
 }
--- src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
+++ src/main/java/com/example/algamoney/api/resource/LancamentoResource.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -53,9 +55,15 @@ public class LancamentoResource {
 	 */
 	
 	// pesquisa de lançamento usando filtro, metadados do jpa
+	/*
+	 * @GetMapping public List<Lancamento> pesquisar(LancamentoFilter
+	 * lancamentoFilter) { return lancamentoRepository.filtrar(lancamentoFilter); }
+	 */
+	
+	// pesquisa de lançamentos usando paginação
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
-		return lancamentoRepository.filtrar(lancamentoFilter);
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 	
 	@GetMapping("/{codigo}")
