b27c66b39e49e8e3c3b12228ed6891c31ac6bfa5
Eliezer <eliezer1789j@gmail.com>
2020-01-07 22:31:26 +0000

    implementando pesquisa de lançamento com Metamodel

--- src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
+++ src/main/java/com/example/algamoney/api/repository/LancamentoRepository.java
@@ -1,9 +1,14 @@
 package com.example.algamoney.api.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.algamoney.api.model.Lancamento;
+import com.example.algamoney.api.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+	List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
 
 }
